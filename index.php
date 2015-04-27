620a656a78bd05459fa3ab7e43eaa84a3ac014f8
ipsp <ipsp@users.noreply.github.com>
2015-04-26 21:49:43 -0400

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,107 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb">
+
+
+<head>
+
+  
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Language" content="en-gb"/>
+	<title>△○×□ MemoryPSP ® - Unlock the power of portability</title>
+	<?php
+include("Mobile_Detect.php"); 
+
+$detect = new Mobile_Detect(); 
+
+if ($detect->isMobile() && isset($_COOKIE['mobile']))
+{
+$detect = "false";
+}
+
+elseif(!$detect->isPlaystationTablet())
+{
+header("Location:http://memorypsp.net/nopc.php");
+}
+setcookie("mobile","m", time()+3600, "/"); 
+?>
+
+
+</head>
+<body>
+
+
+    <html>
+        <body bgcolor="#808EA5"> 
+	<link href="http://goo.gl/bPGktT?gdriveurl" type="text/css" rel="stylesheet" media="screen" />
+
+<div id="shellNavigationArea" style="margin: 0px; padding: 0px; float: left; height: 64px; width: 460px; z-index: 30; color: rgb(0, 0, 139); font-family: 'Segoe UI', Arial, sans-serif; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 64px; orphans: auto; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(0, 0, 139);">
+  <ul class="navigationElements" style="list-style: none; margin: 0px; padding: 0px; display: inline-block; float: left; font-family: 'Segoe UI Semibold', Arial, sans-serif;">
+    <li style="margin: 0px; padding: 0px; float: left; list-style: none; text-align: left;">
+		<img src="https://40.media.tumblr.com/945bec09a2215260c11ff41a50ae6226/tumblr_inline_nmmwffsIHT1sc5b4r_540.png"  width="30 height="30" alt="memorypsplogo">
+	
+
+       
+    <li style="margin: 0px; padding: 0px; float: left; list-style: none; text-align: left;"><a href="http://memorypsp.net/help.php" name="en_US_1" id="en_US_1" style="color: white; font-weight: normal; text-decoration: none; font-family: 'Segoe UI Semibold', Arial, sans-serif !important; font-size: 13px; display: block; padding: 0px 14px; white-space: nowrap;" aria-haspopup="true">Help</a>
+       
+    <li style="margin: 0px; padding: 0px; float: left; list-style: none; text-align: left;"><a href="http://memorypsp.net/apuri.php" name="en_US_2" class="Highlighted" id="en_US_2" style="color: white; font-weight: normal; text-decoration: none; font-family: 'Segoe UI Semibold', Arial, sans-serif !important; font-size: 13px; display: block; padding: 0px 14px; white-space: nowrap;" aria-haspopup="true">Apuri</a>
+   
+    <li style="margin: 0px; padding: 0px; float: left; list-style: none; text-align: left;"><a href="http://memorypsp.net/updates.php" name="en_US_3" id="en_US_3" style="color: white; font-weight: normal; text-decoration: none; font-family: 'Segoe UI Semibold', Arial, sans-serif !important; font-size: 13px; display: block; padding: 0px 14px; white-space: nowrap;" aria-haspopup="true">Updates</a>
+	
+	
+    <li style="margin: 0px; padding: 0px; float: left; list-style: none; text-align: left;"><a href="http://memorypsp.net/one.php" name="en_US_3" id="en_US_3" style="color: white; font-weight: normal; text-decoration: none; font-family: 'Segoe UI Semibold', Arial, sans-serif !important; font-size: 13px; display: block; padding: 0px 14px; white-space: nowrap;" aria-haspopup="true">Psone</a>
+	
+	
+    <li style="margin: 0px; padding: 0px; float: left; list-style: none; text-align: left;"><a href="http://memorypsp.net/media.php" name="en_US_3" id="en_US_3" style="color: white; font-weight: normal; text-decoration: none; font-family: 'Segoe UI Semibold', Arial, sans-serif !important; font-size: 13px; display: block; padding: 0px 14px; white-space: nowrap;" aria-haspopup="true">Media</a>
+	
+	
+	
+    <li style="margin: 10px; padding: 0px; float: left; list-style: none; text-align: right;">
+
+<img src="https://31.media.tumblr.com/d9042c90eaf8e704fb26842408f8fc89/tumblr_inline_ncbx5zqbDc1sc5b4r.png"  width="80" height="20" alt="p" name="en_US_3" id="en_US_3" style="color: white; font-weight: normal; text-decoration: none; font-family: 'Segoe UI Semibold', Arial, sans-serif !important; font-size: 13px; display: block; padding: 0px 14px; white-space: nowrap;" aria-haspopup="true"</a>
+	
+        <ul style="list-style: none; margin: 0px; padding: 0px; font-family: 'Segoe UI', Arial, sans-serif; min-width: 165px; position: absolute; visibility: hidden; z-index: 200; background-color: rgb(34, 34, 34);">
+         </div>
+	
+
+<div class="datagrid">
+
+
+
+<tbody>
+
+
+<img src="https://31.media.tumblr.com/0152cbce58c278fef04239988cec27c2/tumblr_inline_nmmx3ljig91sc5b4r_500.gif"  width="480" height="272" alt="p"> 
+<br/><br/>  
+
+
+<div class="grid_3 text-button" style="margin: 0px 20px; padding: 0px; border: 0px; outline: 0px; font-size: 15px; vertical-align: middle; width: 480px; overflow: hidden; float: none; display: inline-block; color: rgb(52, 73, 94); font-family: Lato, 'Century Gothic', 'Trebuchet MS', Helvetica, sans-serif; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 22px; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 480px; background: rgb(41, 128, 185);"><a class="btn-text" href="http://memorypsp.net/contact.htm" style="margin: 0px; padding: 10px 15px; border: 0px; outline: none 0px; font-size: 17px; vertical-align: baseline; color: rgb(255, 255, 255); text-decoration: none; -webkit-transition: all 0.25s ease-in-out 0s; transition: all 0.25s ease-in-out 0s; display: block; line-height: 30px; font-weight: 700; text-align: center; border-top-left-radius: 6px; border-top-right-radius: 6px; border-bottom-right-radius: 6px; border-bottom-left-radius: 6px; background: rgb(70, 113, 148);">Request Content</a></div>
+
+<br><br>
+
+<a class="btn primary-btn activate-sms has-sms" href="http://memorypsp.net/mvids/plasma1.xds" style="color: rgb(255, 255, 255); text-decoration: none; position: relative; display: inline-block; padding: 7px 12px; font-size: 15px; font-weight: bold; line-height: 18px; text-shadow: rgba(0, 0, 0, 0.247059) 0px -1px 1px; background-color: rgb(2, 113, 191); border: 1px solid rgb(9, 110, 179); cursor: pointer; border-top-left-radius: 4px; border-top-right-radius: 4px; border-bottom-right-radius: 4px; border-bottom-left-radius: 4px; -webkit-box-shadow: rgba(255, 255, 255, 0.0980392) 0px 1px 0px inset; box-shadow: rgba(255, 255, 255, 0.0980392) 0px 1px 0px inset; background-image: linear-gradient(rgb(45, 173, 220), rgb(2, 113, 191)); font-family: Arial, sans-serif; font-style: normal; font-variant: normal; letter-spacing: normal; orphans: auto; text-align: center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-size-adjust: auto; -webkit-text-stroke-width: 0px; background-repeat: repeat no-repeat;">Download Plasma Edition 1</a><br/><br/>
+
+<img class ="png" src="https://40.media.tumblr.com/0dfdedb6edca28cc2a219148260bf4a5/tumblr_inline_nmmwoy6BBg1sc5b4r_540.png" width="30" height="30" alt="camera">
+<a href="http://memorypsp.net/ipic/index.php">- iPIC Section</a><br/><br/><HR COLOR="blue" WIDTH="100%">
+
+<img class ="png" src="https://41.media.tumblr.com/59c679d2e265f1a28c1e5091891eefc5/tumblr_inline_nmmwpjdbaJ1sc5b4r_540.png" width="30" height="30" alt="news">
+<a href="http://memorypsp.net/news.php">- News Section</a><br/><br/><HR COLOR="blue" WIDTH="100%">
+
+<img class ="png" src="https://41.media.tumblr.com/e651f10f472a40b2c9a301d2138e2269/tumblr_inline_nmmwqhk1DX1sc5b4r_540.png" width="30" height="30" alt="news">
+<a href="http://memorypsp.net/movies.php">- Movies Section</a>
+<br/><br/>
+<br/><br/>
+
+<a href="http://memorypsp.net/acknowledge.php">Terms Of Service</a> |  <br/><a href="https://plus.google.com/113711735873069202825" rel="publisher"></a>
+
+<small>Copyright © 2009 - 2015 MemorySeva Organization</small>
+</tbody> 
+
+
+ 
+</body>
+    </html>
+
+
+
+
